dc93e036b24aa63acba5b95d84cd8e1dd67a0a7c
ALopukhin <79193398186l@gmail.com>
2023-10-05 07:38:43 +0300

    jdbc hw

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -10,7 +10,10 @@ include("L15-structuralPatterns:homework")
 
 include("L16-io:demo")
 include("L16-io:homework")
+include("L17-nio-logging")
 
+include("L18-jdbc:demo")
+include("L18-jdbc:homework")
 pluginManagement {
     val jgitver: String by settings
     val dependencyManagement: String by settings
